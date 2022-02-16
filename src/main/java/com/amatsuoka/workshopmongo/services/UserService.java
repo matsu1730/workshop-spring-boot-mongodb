dbe98a118d9ffea6aa76417c080de12cf688c0df
matsu1730 <alf.matsuoka2000@gmail.com>
2022-02-16 19:03:41 -0300

    method insert and POST

--- src/main/java/com/amatsuoka/workshopmongo/services/UserService.java
+++ src/main/java/com/amatsuoka/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amatsuoka.workshopmongo.domain.User;
+import com.amatsuoka.workshopmongo.dto.UserDTO;
 import com.amatsuoka.workshopmongo.repositories.UserRepository;
 import com.amatsuoka.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
+	}
 }
